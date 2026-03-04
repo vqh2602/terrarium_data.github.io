670273ec6b152cdd7b052e7407b0666a573c46ad
WuangHuy <vuongquanghuy.dev@gmail.com>
2026-03-04 22:10:08 +0700

    chore: add iconsax_flutter package and update asset manifests
    Add the iconsax_flutter package to the project, which includes a new font file.
    Update all related asset manifests (FontManifest.json, AssetManifest.bin, etc.) and build files to include the new package assets.
    This enables the use of Iconsax icons in the Flutter application.

--- trutien/flutter_bootstrap.js
+++ trutien/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3764771320"
+    serviceWorkerVersion: "1342540189"
   }
 });
