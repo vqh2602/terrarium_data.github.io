00561805f21a21906371fbe46566989ecb3fb653
WuangHuy <vuongquanghuy.dev@gmail.com>
2026-04-05 19:22:57 +0700

    feat: add new assets and update version to 1.0.0.b24
    Add new level-up videos for all player classes and a new bot image.
    Update player character images and add video_player_web dependency.
    Increment app version and update service worker cache version.

--- trutien/flutter_bootstrap.js
+++ trutien/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1957687242"
+    serviceWorkerVersion: "1035547739"
   }
 });
