4ca71c9700be4ef161221a116a4544ad6006e1a7
WuangHuy <vuongquanghuy.dev@gmail.com>
2026-04-09 08:46:24 +0700

    chore: add build assets and dependencies for Flutter app
    Add icons, fonts, and WebAssembly files for CanvasKit to support PWA features
    Include hairstyle images, tutorial video, and test assets for app functionality
    Add TensorFlow Lite models for face detection and landmark tracking
    Update FontManifest.json and version.json with app configuration

--- atelier/atelier/flutter_bootstrap.js
+++ atelier/atelier/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"425cfb54d01a9472b3e81d9e76fd63a4a44cfb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2863540925" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1907623257" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
